8ceccaae1bc18c59a83142b747fa9227df69a18a
bingbingkuai <158769251+bingbingkuai@users.noreply.github.com>
2024-03-30 13:21:47 -0400

    Update CalculatorController.java, update div section

--- Chapter08/sample1/src/main/java/com/leszko/calculator/CalculatorController.java
+++ Chapter08/sample1/src/main/java/com/leszko/calculator/CalculatorController.java
@@ -17,9 +17,9 @@ class CalculatorController {
 
       @RequestMapping("/div")
         String div(@RequestParam("a") Integer a,
-                        @RequestParam("b") Integer b) {
+                   @RequestParam("b") Integer b) {
                 String result = b!=0 ? String.valueOf(calculator.div(a, b)) : "Division by 0";
-                calculationRepository.save(new Calculation(a.toString(), b.toString(), result, Timestamp.from(Instant.now())));
-                return result;
+              
+                return String.valueOf(calculator.div(a, b));
         }
 }
