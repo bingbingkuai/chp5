143409a52d6f8031503d5c3bfb4c0765b3973dc8
bingbingkuai <158769251+bingbingkuai@users.noreply.github.com>
2024-03-30 13:17:05 -0400

    Update CalculatorController.java, add div section

--- Chapter08/sample1/src/main/java/com/leszko/calculator/CalculatorController.java
+++ Chapter08/sample1/src/main/java/com/leszko/calculator/CalculatorController.java
@@ -14,4 +14,12 @@ class CalculatorController {
                 @RequestParam("b") Integer b) {
           return String.valueOf(calculator.sum(a, b));
      }
+
+      @RequestMapping("/div")
+        String div(@RequestParam("a") Integer a,
+                        @RequestParam("b") Integer b) {
+                String result = b!=0 ? String.valueOf(calculator.div(a, b)) : "Division by 0";
+                calculationRepository.save(new Calculation(a.toString(), b.toString(), result, Timestamp.from(Instant.now())));
+                return result;
+        }
 }
