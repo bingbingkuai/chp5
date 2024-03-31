04025bc455dd68ca0988043bf84414ade8340823
bingbingkuai <158769251+bingbingkuai@users.noreply.github.com>
2024-03-30 22:38:22 -0400

    Update AcceptanceTest.java remove jacoco code

--- Chapter09/sample3/src/test/java/acceptance/AcceptanceTest.java
+++ Chapter09/sample3/src/test/java/acceptance/AcceptanceTest.java
@@ -4,11 +4,6 @@ import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 import org.junit.runner.RunWith;
 
-jacoco {
-    enabled = true
-    destinationFile = file("$buildDir/jacoco/acceptanceTest.exec")
-}
-
 /** Acceptance Test */
 @RunWith(Cucumber.class)
 @CucumberOptions(features = "classpath:acceptance")
